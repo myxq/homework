d4f197b8ab34ab7e15a1658360789a3913489413
Myxq <myxq@gmail.com>
2019-11-24 19:20:03 +0800

    登录与注册界面及后端部分完成

---
+++ app/src/main/java/edu/neu/homework/SignUpActivity.java
@@ -0,0 +1,132 @@
+package edu.neu.homework;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
+
+    @Bind(R.id.input_name)
+    EditText inputName;
+    @Bind(R.id.input_email)
+    EditText inputEmail;
+    @Bind(R.id.input_password)
+    EditText inputPassword;
+    @Bind(R.id.btn_signup)
+    AppCompatButton btnSignUp;
+    @Bind(R.id.link_login)
+    TextView linkLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+
+        linkLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                finish();
+            }
+        });
+    }
+
+    public void signUp() {
+        Log.d(TAG, "SignUp");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+       btnSignUp.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this,
+                R.style.Theme_AppCompat_DayNight_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+//        String name = inputName.getText().toString();
+//        String email = inputEmail.getText().toString();
+//        String password = inputPassword.getText().toString();
+
+        // TODO: Implement your own signUp logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignUpFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        btnSignUp.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        btnSignUp.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = inputName.getText().toString();
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+            inputName.setError("at least 3 characters");
+            valid = false;
+        } else {
+            inputName.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            inputEmail.setError("enter a valid email address");
+            valid = false;
+        } else {
+            inputEmail.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            inputPassword.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            inputPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+}
---
+++ app/src/main/java/edu/neu/homework/gson/User.java
@@ -0,0 +1,16 @@
+package edu.neu.homework.gson;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+
+}
